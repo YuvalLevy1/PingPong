d6934a6ff366e163b77579e29c57bedd4df82b4b
yuvalLevy1 <yuval124levy1@gmail.com>
2022-01-19 17:28:15 +0200

    Added a listener that returns a socket communicator

---
+++ PingPong/Listeners/SocketListener.cs
@@ -0,0 +1,25 @@
+﻿using Communicators;
+using Communicators.Abstractions;
+using Listeners.Abstractions;
+using System;
+using System.Net.Sockets;
+
+namespace Listeners
+{
+    class SocketListener : IListener
+    {
+        private Socket _listener;
+
+        public SocketListener(Socket listener)
+        {
+            _listener = listener;
+        }
+
+        public ICommunicator Listen()
+        {
+            _listener.Listen();
+            var socket = _listener.Accept();
+            return new SocketCommunicator(socket);
+        }
+    }
+}
