543503a8b973dc435d01c5b166bb66a3f7c78131
yuvalLevy1 <yuval124levy1@gmail.com>
2022-01-19 16:33:06 +0200

    Added a string implementation to a decoder

---
+++ PingPong/DataHandlers/Decoders/StringDecoder.cs
@@ -0,0 +1,13 @@
+﻿using DataHandlers.Decoders.Abstractions;
+using System;
+
+namespace DataHandlers.Decoders
+{
+    class StringDecoder : IDecoder<string>
+    {
+        public string Decode(byte[] info)
+        {
+            return System.Text.Encoding.Default.GetString(info);
+        }
+    }
+}
