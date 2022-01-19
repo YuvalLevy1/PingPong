91d0988fd8927152b8b4c68621548f8975e1d146
yuvalLevy1 <yuval124levy1@gmail.com>
2022-01-19 16:26:53 +0200

    Added implamentation to a string encoder

--- PingPong/DataHandlers/Encoders/Abstractions/IEncoder.cs
+++ PingPong/DataHandlers/Encoders/Abstractions/IEncoder.cs
@@ -1,6 +1,4 @@
 ﻿
-using System;
-
 namespace DataHandlers.Encoders.Abstractions
 {
     public interface IEncoder<T>
---
+++ PingPong/DataHandlers/Encoders/StringEncoder.cs
@@ -0,0 +1,13 @@
+﻿
+using DataHandlers.Encoders.Abstractions;
+
+namespace DataHandlers.Encoders
+{
+    public class StringEncoder : IEncoder<string>
+    {
+        public string Translate(byte[] info)
+        {
+            return System.Text.Encoding.Default.GetString(info);
+        }
+    }
+}
