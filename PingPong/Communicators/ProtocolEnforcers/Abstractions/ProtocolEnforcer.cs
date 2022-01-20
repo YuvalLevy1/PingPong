7e66253b4d966d1cf5a656635aad888c3d162a2d
yuvalLevy1 <yuval124levy1@gmail.com>
2022-01-20 11:30:55 +0200

    Added an abstraction to a protocol enforcer

---
+++ PingPong/Communicators/ProtocolEnforcers/Abstractions/ProtocolEnforcer.cs
@@ -0,0 +1,23 @@
+﻿using Communicators.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Communicators.ProtocolEnforcers.Abstractions
+{
+    public abstract class ProtocolEnforcer
+    {
+        private ICommunicator _communicator;
+
+        protected ProtocolEnforcer(ICommunicator communicator)
+        {
+            _communicator = communicator;
+        }
+
+        public abstract void Send(byte[] info);
+
+        public abstract byte[] Receive();
+    }
+}
