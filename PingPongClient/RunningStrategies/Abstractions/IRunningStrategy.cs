6783f69a601cf9e7e107d8963c76674702db9fd5
yuvalLevy1 <yuval124levy1@gmail.com>
2022-01-20 11:12:50 +0200

    Added an abstraction to a client's running strategy

---
+++ PingPongClient/RunningStrategies/Abstractions/IRunningStrategy.cs
@@ -0,0 +1,12 @@
+﻿using Communicators.Abstractions;
+using DataHandlers.Decoders.Abstractions;
+using DataHandlers.Encoders.Abstractions;
+using Utils;
+
+namespace RunningStrategies.Abstractions
+{
+    interface IRunningStrategy<T>
+    {
+        public void Run(ICommunicator client, ConcurrentHashSet<ICommunicator> clients, IEncoder<T> encoder, IDecoder<T> decoder);
+    }
+}
