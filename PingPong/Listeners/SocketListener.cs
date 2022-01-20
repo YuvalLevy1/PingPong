f63d17fe1053fab9e059ed413f1abb291d1b8d9c
yuvalLevy1 <yuval124levy1@gmail.com>
2022-01-20 16:02:37 +0200

    Uncoupled listener from SizeProtocolEnforcer

--- PingPong/Listeners/SocketListener.cs
+++ PingPong/Listeners/SocketListener.cs
@@ -1,10 +1,10 @@
 ﻿using Communicators;
-using Communicators.Abstractions;
 using Communicators.ProtocolEnforcers;
 using Communicators.ProtocolEnforcers.Abstractions;
 using DataHandlers.Decoders.Abstractions;
 using DataHandlers.Encoders.Abstractions;
 using Listeners.Abstractions;
+using ProtocolEnforcerFactories.Abstractions;
 using System.Net;
 using System.Net.Sockets;
 
@@ -13,21 +13,15 @@ namespace Listeners
     public class SocketListener : IListener
     {
         private Socket _listener;
-        private readonly IEncoder<string> _encoder;
-        private readonly IDecoder<string> _decoder;
-        private readonly int _bufferSize;
-        private readonly int _sizeOfLength;
+        private readonly IProtocolEnforcerFactory _factory;
 
-        public SocketListener(int port, IEncoder<string> encoder, IDecoder<string> decoder, int bufferSize, int sizeOfLength)
+        public SocketListener(int port, IProtocolEnforcerFactory factory)
         {
             var ip = Dns.GetHostEntry(Dns.GetHostName()).AddressList[0];
             var localEndpoint = new IPEndPoint(ip, port);
             _listener = new Socket(localEndpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
             _listener.Bind(localEndpoint);
-            _encoder = encoder;
-            _decoder = decoder;
-            _bufferSize = bufferSize;
-            _sizeOfLength = sizeOfLength;
+            _factory = factory;
         }
 
         public IProtocolEnforcer Listen()
@@ -35,7 +29,7 @@ namespace Listeners
             _listener.Listen();
             var socket = _listener.Accept();
             var socketCommunicator = new SocketCommunicator(socket);
-            return new SizeProtocolEnforcer(socketCommunicator, _encoder, _decoder, 4, 1024);
+            return _factory.Create(socketCommunicator);
         }
 
         public void Close()
